38c6402fd589b9ff3312da8da25d0d986e6fd0f1
MohdAzeem121 <155133733+MohdAzeem121@users.noreply.github.com>
2024-12-31 12:49:38 +0530

    Add files via upload

---
+++ project.net/main.js
@@ -0,0 +1,8 @@
+const burger=document.getElementById("humberger");
+const list=documment.getElementById("navlist");
+
+burger.addEventListener("click ", ()=>{
+    
+    list.classlist.toggle("navlist-active")
+})
+   
